e49522469c2ce81d5061b0548c8b58b305d0885c
jiy8ni <cindy030609@gmail.com>
2023-04-11 11:06:05 +0900

    Make hello

---
+++ 0411-project/hello/src/pages/world.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+export default function World(props) {
+    return <span style={{ fontWeight : "bold"}}>
+        {props.name}!</span>
+}
+
+// props : properties -> 컴포넌트에서 properties
